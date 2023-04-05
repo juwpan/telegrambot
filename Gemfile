56ec1de79f91d05bcc538d07dfa729e54506185a
juwpan <juwpan@gmail.com>
2023-04-05 10:03:31 +0400

    added sinatra and deploy file

--- Gemfile
+++ Gemfile
@@ -4,3 +4,9 @@ source "https://rubygems.org"
 
 gem 'telegram-bot-ruby', '~> 1.0'
 gem 'dotenv-rails'
+gem 'sinatra'
+gem 'puma'
+
+gem "capistrano"
+gem "capistrano-bundler"
+gem "capistrano-passenger"
--- main.rb
+++ main.rb
@@ -10,31 +10,33 @@ FOLDER_ID = ENV['FOLDER_ID']
 IAM_TOKEN = ENV['IAM_TOKEN']
 
 Telegram::Bot::Client.run(TOKEN, { polling: true }) do |bot|
-  bot.listen do |message|
-    if message.voice
-      # Извлечение голосового файла
-      file_id = message.voice.file_id
-      file_path = bot.api.get_file(file_id: file_id).fetch('result').fetch('file_path')
-      voice_url = "https://api.telegram.org/file/bot#{TOKEN}/#{file_path}"
-      uri = URI(voice_url)
-      response = Net::HTTP.get_response(uri)
+  get '/' do
+    bot.listen do |message|
+      if message.voice
+        # Извлечение голосового файла
+        file_id = message.voice.file_id
+        file_path = bot.api.get_file(file_id: file_id).fetch('result').fetch('file_path')
+        voice_url = "https://api.telegram.org/file/bot#{TOKEN}/#{file_path}"
+        uri = URI(voice_url)
+        response = Net::HTTP.get_response(uri)
 
-      # Отправка для преобразования yandex spechkit
-      uri = URI("https://stt.api.cloud.yandex.net/speech/v1/stt:recognize?folderId=#{FOLDER_ID}&lang=ru-RU")
-      request = Net::HTTP::Post.new(uri)
-      request['Authorization'] = "Bearer #{IAM_TOKEN}"
-      request.body = response.body
-      response = Net::HTTP.start(uri.hostname, uri.port, use_ssl: true) do |http|
-        http.request(request)
-      end
+        # Отправка для преобразования yandex spechkit
+        uri = URI("https://stt.api.cloud.yandex.net/speech/v1/stt:recognize?folderId=#{FOLDER_ID}&lang=ru-RU")
+        request = Net::HTTP::Post.new(uri)
+        request['Authorization'] = "Bearer #{IAM_TOKEN}"
+        request.body = response.body
+        response = Net::HTTP.start(uri.hostname, uri.port, use_ssl: true) do |http|
+          http.request(request)
+        end
 
-      # Вывод результата пользователю в форме текста
-      recognized_text = JSON.parse(response.body)['result']
+        # Вывод результата пользователю в форме текста
+        recognized_text = JSON.parse(response.body)['result']
 
-      if recognized_text.empty?
-        bot.api.send_message(chat_id: message.chat.id, text: "Ничего не слышу.")
-      else
-        bot.api.send_message(chat_id: message.chat.id, text: recognized_text)
+        if recognized_text.empty?
+          bot.api.send_message(chat_id: message.chat.id, text: "Ничего не слышу.")
+        else
+          bot.api.send_message(chat_id: message.chat.id, text: recognized_text)
+        end
       end
     end
   end
