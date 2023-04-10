4679df34b93c80627ff9582412655b249f6d7ba7
juwpan <juwpan@gmail.com>
2023-04-10 11:42:16 +0400

    added module response_yandex for get response from yandex cloud

---
+++ lib/response_yandex.rb
@@ -0,0 +1,20 @@
+module ResponseYandex
+  def self.get_response_form_object(url, headers)
+    response = Net::HTTP.get_response(url, headers)
+    result = JSON.parse(response.body)
+
+    while result['done'] != true
+      if result['done'] == true
+        p 'Connect'
+        return result
+      else
+        response = Net::HTTP.get_response(url, headers)
+        result = JSON.parse(response.body)
+        p 'No connect'
+        sleep 1
+      end
+    end
+
+    result
+  end
+end
